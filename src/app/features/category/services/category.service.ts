d34b8e38ee63b7b2364f4d803f806c3a4e350ba2
nenadla <nenadgf@gmail.com>
2024-07-16 21:14:58 +0200

    Edit: Create Service Method and Display Single Category

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -20,4 +20,8 @@ export class CategoryService {
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
   }
 
+  getCategoryById(id: string): Observable<Category>{
+     return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+  }
+
 }
